322c9ac9eb0f3c8d8088c98851fbe31adf931987
scottschmidl <sjschmidl@gmail.com>
2024-02-12 21:56:40 -0600

    started separate file for load and preprocessing

---
+++ cs7641_machine_learning/lectures/week9/assignment2/source/wrangle.py
@@ -0,0 +1,49 @@
+# manipulate data
+import numpy as np
+import pandas as pd
+
+# visualize data
+import matplotlib.pyplot as plt
+import seaborn as sn
+
+# model preparation
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+def clean(dataset=None:str):
+    match dataset:
+        
+        case "cvd":
+            pass
+
+        case "nf":
+            pass
+
+    
+    return df_cleaned
+
+def scale(cleaned=None:str):
+
+    match cleaned:
+        
+        case 'cvd':
+            pass
+
+        case 'nf':
+            pass
+        
+    return df_scaled
+
+def final_dataset(scaled=None:str):
+
+    match scaled:
+        
+        case 'cvd':
+            pass
+
+        case 'nf':
+            pass
+
+        return df_final
+    

b846ae2d69966cce3f5029caadbe13562eb5835c
scottschmidl <sjschmidl@gmail.com>
2024-02-13 23:48:11 -0600

    changed file name and prep code for opt prob

--- cs7641_machine_learning/lectures/week9/assignment2/source/models.py
+++ cs7641_machine_learning/lectures/week9/assignment2/source/models.py
@@ -12,13 +12,11 @@ import mlrose_hiive as rh
 
 # model evaluation
 from sklearn.metrics import (balanced_accuracy_score,
-                             make_scorer,
-                             recall_score,
-                             log_loss)
+                             recall_score)
 
 np.random.seed(123)
 
-def weights_algo(algo):
+def get_nn_algo(algo):
     """Instantiate NN Algos
 
     Args:
@@ -64,11 +62,11 @@ def weights_algo(algo):
                                   mutation_prob=0.1, 
                                   max_iters=300, 
                                   random_state=123, 
-                                  curve=True)
+                                  curve=True)            
             
     return nn
 
-def optimize_weights(X_train, y_train, X_test, y_test):
+def good_weights(X_train, y_train, X_test, y_test):
     """Uses selected algorithm to search for optimal NN weights
 
     Args:
@@ -77,21 +75,21 @@ def optimize_weights(X_train, y_train, X_test, y_test):
 
     algos = np.array(['Random Hill Climbing',
                       'Simulated Annealing',
-                      'Genetic Algorithm'])
+                      'Genetic Algorithm',])
 
     print('Running All Algorithms')
-    nn = {algo: {'model': weights_algo(algo=algo)} for algo in algos}
+    nn = {algo: {'nn': get_nn_algo(algo=algo)} for algo in algos}
     print('Successfully Run All Algorithms')
 
     for algo in algos:
         print(f'\nWorking on: {algo}\nFitting: {algo}')
-        nn[algo]['model'].fit(X_train, y_train)
+        nn[algo]['nn'].fit(X_train, y_train)
 
         # TRAINING
         print(f'\nPredicting (Training): {algo}')
-        y_train_pred = nn[algo]['model'].predict(X_train)
+        y_train_pred = nn[algo]['nn'].predict(X_train)
 
-        print(f'\Calculating Recall (Training): {algo}')
+        print(f'\nCalculating Recall (Training): {algo}')
         recall_train = recall_score(y_train, y_train_pred)
 
         print(f'\nAppending Results (Training): {algo}')
@@ -101,34 +99,47 @@ def optimize_weights(X_train, y_train, X_test, y_test):
         print(f"\nThe train recall score: {recall_train}%")
 
         # TESTING
-        # print(f'\nPredicting (Testing): {algo}')
-        # y_test_pred = nn[algo]['model'].predict(X_test)
+        print(f'\nPredicting (Testing): {algo}')
+        y_test_pred = nn[algo]['nn'].predict(X_test)
 
-        # print(f'\Calculting Recall (Testing): {algo}')
-        # recall_test = recall_score(y_test, y_test_pred)
+        print(f'\nCalculting Recall (Testing): {algo}')
+        recall_test = recall_score(y_test, y_test_pred)
 
-        # print(f'\nAppending Results (Testing): {algo}')
-        # nn[algo]['y_train_pred'] = y_train_pred
-        # nn[algo]['recall_train'] = recall_train
+        print(f'\nAppending Results (Testing): {algo}')
+        nn[algo]['y_test_pred'] = y_test_pred
+        nn[algo]['recall_test'] = recall_test
         
-        # print(f"\nThe test recall score: {recall_train}%")
-            
-        # # Create/display fitness curve plot for selected algorithm.
-        # plt.plot(nn.fitness_curve)
-        # plt.ylabel('Relative Fitness Found')
-        # plt.show()
+        print(f"\nThe test recall score: {recall_test}%")
+
+        return nn
+
+def get_op_algo(algo):
+    pass
+
+def good_problem(algo):
+
+    algos = np.array(['Random Hill Climbing'
+                      'Simulated Annealing',
+                      'Genetic Algorithm',
+                      'mimic'])
+
+    problems = np.array([])
+
+    print('Running All Algorithms')
+    opt_prob = {algo: {problem: get_op_algo(algo=algo)} for algo in algos for problem in problems}
+    print('Successfully Run All Algorithms')
+
+    return op
+    pass
 
 def main():
 
     X_train_scaled_cd, X_test_scaled_cd, y_train_cd, y_test_cd = final_dataset(dataset='cvd')
 
-    optimize_weights(X_train_scaled_cd, y_train_cd,
-                     X_test_scaled_cd, y_test_cd)
+    # nn = good_weights(X_train_scaled_cd, y_train_cd,
+    #                  X_test_scaled_cd, y_test_cd)
 
-    # X_train_scaled_nf, X_test_scaled_nf, y_train_nf, y_test_nf = final_dataset(dataset='nf')
-
-    # optimize_weights(X_train_scaled_nf, y_train_nf,
-    #                  X_test_scaled_nf, y_test_nf)
+    # op = algo_opt_problem()
 
 if __name__ == "__main__":
     main()

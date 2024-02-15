3c1a6aff3414bbb5284b73281952f98c796f3eb8
scottschmidl <sjschmidl@gmail.com>
2024-02-14 23:40:13 -0600

    update models with opt prob code and added gradient decent for nn

--- cs7641_machine_learning/lectures/week9/assignment2/source/models.py
+++ cs7641_machine_learning/lectures/week9/assignment2/source/models.py
@@ -11,8 +11,7 @@ import matplotlib.pyplot as plt
 import mlrose_hiive as rh
 
 # model evaluation
-from sklearn.metrics import (balanced_accuracy_score,
-                             recall_score)
+from sklearn.metrics import recall_score
 
 np.random.seed(123)
 
@@ -37,7 +36,7 @@ def get_nn_algo(algo):
                                   restarts=200,
                                   max_iters=300,
                                   random_state=123,
-                                  curve=True)
+                                  curve=True,)
             
         case 'Simulated Annealing':
             print(f"Running {algo}")
@@ -49,7 +48,7 @@ def get_nn_algo(algo):
                                   schedule=rh.GeomDecay(), 
                                   max_iters=300,
                                   random_state=123,
-                                  curve = True)
+                                  curve = True,)
             
         case 'Genetic Algorithm':
             print(f"Running {algo}")
@@ -62,7 +61,19 @@ def get_nn_algo(algo):
                                   mutation_prob=0.1, 
                                   max_iters=300, 
                                   random_state=123, 
-                                  curve=True)            
+                                  curve=True,)
+
+        case 'Gradient Descent':
+            print(f"Running {algo}")
+            
+            nn = rh.NeuralNetwork(hidden_nodes=[4], 
+                                  activation='relu',
+                                  algorithm='gradient_descent', 
+                                  clip_max=1, 
+                                  pop_size=200, 
+                                  max_iters=300, 
+                                  random_state=123, 
+                                  curve=True,)   
             
     return nn
 
@@ -75,7 +86,8 @@ def good_weights(X_train, y_train, X_test, y_test):
 
     algos = np.array(['Random Hill Climbing',
                       'Simulated Annealing',
-                      'Genetic Algorithm',])
+                      'Genetic Algorithm',
+                      'Gradient Descent'])
 
     print('Running All Algorithms')
     nn = {algo: {'nn': get_nn_algo(algo=algo)} for algo in algos}
@@ -109,37 +121,185 @@ def good_weights(X_train, y_train, X_test, y_test):
         nn[algo]['y_test_pred'] = y_test_pred
         nn[algo]['recall_test'] = recall_test
         
-        print(f"\nRecall Score (TESTING): {recall_test}%")
+        print(f"\nRecall Score (TESTING): {recall_test}%\n")
 
     return nn
 
 def get_op_algo(algo):
-    pass
 
-def good_problem(algo):
+    match algo:
 
-    algos = np.array(['Random Hill Climbing'
-                      'Simulated Annealing',
-                      'Genetic Algorithm',
-                      'mimic'])
+        case 'Random Hill Climbing':
+            print(f"Running {algo}")
 
-    problems = np.array([])
+            # Four Peaks
+            fitness_fnc_FP = rh.FourPeaks()
+            op_type_FP = rh.DiscreteOpt(length=4, fitness_fn=fitness_fnc_FP,
+                                        maximize=True,
+                                        max_val=2)
+            
+            best_state_FP, best_fitness_FP, fitness_curve_FP = rh.random_hill_climb(problem=op_type_FP,
+                                                                                 random_state=123,
+                                                                                 curve=True)
+
+            # FlipFlop
+            fitness_fnc_FF = rh.FlipFlop()
+            op_type_FF = rh.DiscreteOpt(length=4, fitness_fn=fitness_fnc_FF,
+                                       maximize=True,
+                                       max_val=2)
+            
+            best_state_FF, best_fitness_FF, fitness_curve_FF = rh.random_hill_climb(problem=op_type_FF,
+                                                                               random_state=123,
+                                                                               curve=True)
+
+            # Knapsack
+            fitness_fnc_KS = rh.Knapsack(weights=[0.25, 0.50, 0.75, 1.0],
+                                         values=[2.5, 5.0, 7.5, 10])
+            op_type_KS = rh.DiscreteOpt(length=4, fitness_fn=fitness_fnc_KS,
+                                         maximize=True,
+                                         max_val=2)
+
+            best_state_KS, best_fitness_KS, fitness_curve_KS = rh.random_hill_climb(problem=op_type_KS,
+                                                                                   random_state=123,
+                                                                                   curve=True)
+            
+        case 'Simulated Annealing':
+            print(f"Running {algo}")
+
+            # Four Peaks
+            fitness_fnc_FP = rh.FourPeaks()
+            op_type_FP = rh.DiscreteOpt(length=4, fitness_fn=fitness_fnc_FP,
+                                        maximize=True,
+                                        max_val=2)
+
+            best_state_FP, best_fitness_FP, fitness_curve_FP = rh.simulated_annealing(problem=op_type_FP,
+                                                                                   random_state=123, 
+                                                                                   curve=True)
+
+            # FlipFlop
+            fitness_fnc_FF = rh.FlipFlop()
+            op_type_FF = rh.DiscreteOpt(length=4, fitness_fn=fitness_fnc_FF,
+                                       maximize=True,
+                                       max_val=2)
+
+            best_state_FF, best_fitness_FF, fitness_curve_FF = rh.simulated_annealing(problem=op_type_FF,
+                                                                                random_state=123, 
+                                                                                curve=True)
+            
+            # Knapsack
+            fitness_fnc_KS = rh.Knapsack(weights=[0.25, 0.50, 0.75, 1.0],
+                                         values=[2.5, 5.0, 7.5, 10])
+            op_type_KS = rh.DiscreteOpt(length=4, fitness_fn=fitness_fnc_KS,
+                                         maximize=True,
+                                         max_val=2)
+
+            best_state_KS, best_fitness_KS, fitness_curve_KS = rh.simulated_annealing(problem=op_type_KS,
+                                                                                    random_state=123, 
+                                                                                    curve=True)
+            
+        case 'Genetic Algorithm':
+            print(f"Running {algo}")
+
+            # Four Peaks
+            fitness_fnc_FP = rh.FourPeaks()
+            op_type_FP = rh.DiscreteOpt(length=4, fitness_fn=fitness_fnc_FP,
+                                        maximize=True,
+                                        max_val=2)
+
+            best_state_FP, best_fitness_FP, fitness_curve_FP = rh.genetic_alg(problem=op_type_FP,
+                                                                           random_state=123,
+                                                                           curve=True)
+
+            # FlipFlop
+            fitness_fnc_FF = rh.FlipFlop()
+            op_type_FF = rh.DiscreteOpt(length=4, fitness_fn=fitness_fnc_FF,
+                                        maximize=True,
+                                        max_val=2)
+
+            best_state_FF, best_fitness_FF, fitness_curve_FF = rh.genetic_alg(problem=op_type_FF,
+                                                                         random_state=123,
+                                                                         curve=True)
+
+            # Knapsack
+            fitness_fnc_KS = rh.Knapsack(weights=[0.25, 0.50, 0.75, 1.0],
+                                         values=[2.5, 5.0, 7.5, 10])
+            op_type_KS = rh.DiscreteOpt(length=4, fitness_fn=fitness_fnc_KS,
+                                        maximize=True,
+                                        max_val=2)
+
+            best_state_KS, best_fitness_KS, fitness_curve_KS = rh.genetic_alg(problem=op_type_KS,
+                                                                             random_state=123,
+                                                                             curve=True)
+
+        case 'MIMIC':
+            print(f"Running {algo}")
+
+            # Four Peaks
+            fitness_fnc_FP = rh.FourPeaks()
+            op_type_FP = rh.DiscreteOpt(length=4, fitness_fn=fitness_fnc_FP,
+                                        maximize=True,
+                                        max_val=2)
+
+            best_state_FP, best_fitness_FP, fitness_curve_FP = rh.mimic(problem=op_type_FP,
+                                                                     random_state=123,
+                                                                     curve=True)
+
+            # FlipFlop
+            fitness_fnc_FF = rh.FlipFlop()
+            op_type_FF = rh.DiscreteOpt(length=4, fitness_fn=fitness_fnc_FF,
+                                       maximize=True,
+                                       max_val=2)
+
+            best_state_FF, best_fitness_FF, fitness_curve_FF = rh.mimic(problem=op_type_FF,
+                                                                   random_state=123,
+                                                                   curve=True)
+
+            # Knapsack
+            fitness_fnc_KS = rh.Knapsack(weights=[0.25, 0.50, 0.75, 1.0],
+                                         values=[2.5, 5.0, 7.5, 10])
+            op_type_KS = rh.DiscreteOpt(length=4, fitness_fn=fitness_fnc_KS,
+                                         maximize=True,
+                                         max_val=2)
+
+            best_state_KS, best_fitness_KS, fitness_curve_KS = rh.mimic(problem=op_type_KS,
+                                                                       random_state=123,
+                                                                       curve=True)
+            
+    best_state =  {'best_state_FP': best_state_FP,
+                   'best_state_FF': best_state_FF,
+                   'best_state_KS': best_state_KS}
+
+    best_fitness = {'best_fitness_FP': best_fitness_FP,
+                    'best_fitness_FF': best_fitness_FF,
+                    'best_fitness_KS': best_fitness_KS}
+
+    fitness_curve = {'fitness_curve_FP': fitness_curve_FP,
+                     'fitness_curve_FF': fitness_curve_FF,
+                     'fitness_curve_KS': fitness_curve_KS}
+    
+    return best_state, best_fitness, fitness_curve
+
+def good_problem():
+
+    algos = np.array(['Random Hill Climbing',
+             'Simulated Annealing',
+             'Genetic Algorithm',
+             'MIMIC'])
 
     print('Running All Algorithms')
-    opt_prob = {algo: {problem: get_op_algo(algo=algo)} for algo in algos for problem in problems}
+    opt_prob = {algo: get_op_algo(algo=algo) for algo in algos}
     print('Successfully Run All Algorithms')
 
-    return op
-    pass
+    return opt_prob
 
 def main():
 
     X_train_scaled_cd, X_test_scaled_cd, y_train_cd, y_test_cd = final_dataset(dataset='cvd')
 
     # nn = good_weights(X_train_scaled_cd, y_train_cd,
-    #                  X_test_scaled_cd, y_test_cd)
+    #                   X_test_scaled_cd, y_test_cd)
 
-    # op = algo_opt_problem()
+    # opt_prob = good_problem()
 
 if __name__ == "__main__":
     main()

05ca795737f1d26b3d5e5b35f444b699a6738e79
scottschmidl <sjschmidl@gmail.com>
2024-02-15 20:18:02 -0600

    split up nn and ro workflows

---
+++ cs7641_machine_learning/lectures/week9/assignment2/source/modelsNN.py
@@ -0,0 +1,135 @@
+# load and preprocess
+from wrangle import final_dataset
+
+# manipulate data
+import numpy as np
+
+# machine learning models
+import mlrose_hiive as rh
+
+# model evaluation
+from sklearn.metrics import recall_score
+
+np.random.seed(123)
+
+def get_nn_algo(algo):
+    """Instantiate NN Algos
+
+    Args:
+        algo (str): Which algo to use with NN
+
+    Returns:
+        NN Object: NN object for algo
+    """
+
+    match algo:
+        case 'Random Hill Climbing':
+            print(f"Running {algo}")
+            
+            nn = rh.NeuralNetwork(hidden_nodes=[4],
+                                  activation='relu',
+                                  algorithm='random_hill_climb',
+                                  clip_max=1,
+                                  restarts=200,
+                                  max_iters=300,
+                                  random_state=123,
+                                  curve=True,)
+            
+        case 'Simulated Annealing':
+            print(f"Running {algo}")
+            
+            nn = rh.NeuralNetwork(hidden_nodes=[4],
+                                  activation='relu',
+                                  algorithm='simulated_annealing',
+                                  clip_max=1,
+                                  schedule=rh.GeomDecay(), 
+                                  max_iters=300,
+                                  random_state=123,
+                                  curve = True,)
+            
+        case 'Genetic Algorithm':
+            print(f"Running {algo}")
+            
+            nn = rh.NeuralNetwork(hidden_nodes=[4], 
+                                  activation='relu',
+                                  algorithm='genetic_alg', 
+                                  clip_max=1, 
+                                  pop_size=200,
+                                  mutation_prob=0.1, 
+                                  max_iters=300, 
+                                  random_state=123, 
+                                  curve=True,)
+
+        case 'Gradient Descent':
+            print(f"Running {algo}")
+            
+            nn = rh.NeuralNetwork(hidden_nodes=[4], 
+                                  activation='relu',
+                                  algorithm='gradient_descent', 
+                                  clip_max=1, 
+                                  pop_size=200, 
+                                  max_iters=300, 
+                                  random_state=123, 
+                                  curve=True,)   
+            
+    return nn
+
+def good_weights(X_train, y_train, X_test, y_test):
+    """Uses selected algorithm to search for optimal NN weights
+
+    Args:
+        data (pd.DataFrame): data to use for optimization
+    """
+
+    algos = np.array(['Random Hill Climbing',
+                      'Simulated Annealing',
+                      'Genetic Algorithm',
+                      'Gradient Descent'])
+
+    print('Running All Algorithms')
+    nn = {algo: {'nn': get_nn_algo(algo=algo)} for algo in algos}
+    print('Successfully Run All Algorithms')
+
+    for algo in algos:
+        print(f'\nWorking on: {algo}\nFitting: {algo}')
+        nn[algo]['nn'].fit(X_train, y_train)
+
+        # TRAINING
+        print(f'\nPredicting (TRAINING): {algo}')
+        y_train_pred = nn[algo]['nn'].predict(X_train)
+
+        print(f'\nCalculating Recall (TRAINING): {algo}')
+        recall_train = recall_score(y_train, y_train_pred)
+
+        print(f'\nAppending Results (TRAINING): {algo}')
+        nn[algo]['y_train_pred'] = y_train_pred
+        nn[algo]['recall_train'] = recall_train
+        
+        print(f"\nRecall Score (TRAINING): {recall_train}%")
+
+        # TESTING
+        print(f'\nPredicting (TESTING): {algo}')
+        y_test_pred = nn[algo]['nn'].predict(X_test)
+
+        print(f'\nCalculting Recall (TESTING): {algo}')
+        recall_test = recall_score(y_test, y_test_pred)
+
+        print(f'\nAppending Results (TESTING): {algo}')
+        nn[algo]['y_test_pred'] = y_test_pred
+        nn[algo]['recall_test'] = recall_test
+        
+        print(f"\nRecall Score (TESTING): {recall_test}%\n")
+
+    return nn
+
+
+
+def main():
+
+    X_train_scaled_cd, X_test_scaled_cd, y_train_cd, y_test_cd = final_dataset(dataset='cvd')
+
+    # nn = good_weights(X_train_scaled_cd, y_train_cd,
+    #                   X_test_scaled_cd, y_test_cd)
+
+if __name__ == "__main__":
+    main()
--- cs7641_machine_learning/lectures/week9/assignment2/source/modelsRO.py
+++ cs7641_machine_learning/lectures/week9/assignment2/source/modelsRO.py
@@ -4,126 +4,10 @@ from wrangle import final_dataset
 # manipulate data
 import numpy as np
 
-# visualize data
-import matplotlib.pyplot as plt
-
 # machine learning models
 import mlrose_hiive as rh
 
-# model evaluation
-from sklearn.metrics import recall_score
-
 np.random.seed(123)
-
-def get_nn_algo(algo):
-    """Instantiate NN Algos
-
-    Args:
-        algo (str): Which algo to use with NN
-
-    Returns:
-        NN Object: NN object for algo
-    """
-
-    match algo:
-        case 'Random Hill Climbing':
-            print(f"Running {algo}")
-            
-            nn = rh.NeuralNetwork(hidden_nodes=[4],
-                                  activation='relu',
-                                  algorithm='random_hill_climb',
-                                  clip_max=1,
-                                  restarts=200,
-                                  max_iters=300,
-                                  random_state=123,
-                                  curve=True,)
-            
-        case 'Simulated Annealing':
-            print(f"Running {algo}")
-            
-            nn = rh.NeuralNetwork(hidden_nodes=[4],
-                                  activation='relu',
-                                  algorithm='simulated_annealing',
-                                  clip_max=1,
-                                  schedule=rh.GeomDecay(), 
-                                  max_iters=300,
-                                  random_state=123,
-                                  curve = True,)
-            
-        case 'Genetic Algorithm':
-            print(f"Running {algo}")
-            
-            nn = rh.NeuralNetwork(hidden_nodes=[4], 
-                                  activation='relu',
-                                  algorithm='genetic_alg', 
-                                  clip_max=1, 
-                                  pop_size=200,
-                                  mutation_prob=0.1, 
-                                  max_iters=300, 
-                                  random_state=123, 
-                                  curve=True,)
-
-        case 'Gradient Descent':
-            print(f"Running {algo}")
-            
-            nn = rh.NeuralNetwork(hidden_nodes=[4], 
-                                  activation='relu',
-                                  algorithm='gradient_descent', 
-                                  clip_max=1, 
-                                  pop_size=200, 
-                                  max_iters=300, 
-                                  random_state=123, 
-                                  curve=True,)   
-            
-    return nn
-
-def good_weights(X_train, y_train, X_test, y_test):
-    """Uses selected algorithm to search for optimal NN weights
-
-    Args:
-        data (pd.DataFrame): data to use for optimization
-    """
-
-    algos = np.array(['Random Hill Climbing',
-                      'Simulated Annealing',
-                      'Genetic Algorithm',
-                      'Gradient Descent'])
-
-    print('Running All Algorithms')
-    nn = {algo: {'nn': get_nn_algo(algo=algo)} for algo in algos}
-    print('Successfully Run All Algorithms')
-
-    for algo in algos:
-        print(f'\nWorking on: {algo}\nFitting: {algo}')
-        nn[algo]['nn'].fit(X_train, y_train)
-
-        # TRAINING
-        print(f'\nPredicting (TRAINING): {algo}')
-        y_train_pred = nn[algo]['nn'].predict(X_train)
-
-        print(f'\nCalculating Recall (TRAINING): {algo}')
-        recall_train = recall_score(y_train, y_train_pred)
-
-        print(f'\nAppending Results (TRAINING): {algo}')
-        nn[algo]['y_train_pred'] = y_train_pred
-        nn[algo]['recall_train'] = recall_train
-        
-        print(f"\nRecall Score (TRAINING): {recall_train}%")
-
-        # TESTING
-        print(f'\nPredicting (TESTING): {algo}')
-        y_test_pred = nn[algo]['nn'].predict(X_test)
-
-        print(f'\nCalculting Recall (TESTING): {algo}')
-        recall_test = recall_score(y_test, y_test_pred)
-
-        print(f'\nAppending Results (TESTING): {algo}')
-        nn[algo]['y_test_pred'] = y_test_pred
-        nn[algo]['recall_test'] = recall_test
-        
-        print(f"\nRecall Score (TESTING): {recall_test}%\n")
-
-    return nn
 
 def get_op_algo(algo):
 
@@ -140,7 +24,7 @@ def get_op_algo(algo):
             
             best_state_FP, best_fitness_FP, fitness_curve_FP = rh.random_hill_climb(problem=op_type_FP,
                                                                                  random_state=123,
-                                                                                 curve=True)
+                                                                                 curve=True,)
 
             # FlipFlop
             fitness_fnc_FF = rh.FlipFlop()
@@ -150,7 +34,7 @@ def get_op_algo(algo):
             
             best_state_FF, best_fitness_FF, fitness_curve_FF = rh.random_hill_climb(problem=op_type_FF,
                                                                                random_state=123,
-                                                                               curve=True)
+                                                                               curve=True,)
 
             # Knapsack
             fitness_fnc_KS = rh.Knapsack(weights=[0.25, 0.50, 0.75, 1.0],
@@ -161,7 +45,7 @@ def get_op_algo(algo):
 
             best_state_KS, best_fitness_KS, fitness_curve_KS = rh.random_hill_climb(problem=op_type_KS,
                                                                                    random_state=123,
-                                                                                   curve=True)
+                                                                                   curve=True,)
             
         case 'Simulated Annealing':
             print(f"Running {algo}")
@@ -174,7 +58,7 @@ def get_op_algo(algo):
 
             best_state_FP, best_fitness_FP, fitness_curve_FP = rh.simulated_annealing(problem=op_type_FP,
                                                                                    random_state=123, 
-                                                                                   curve=True)
+                                                                                   curve=True,)
 
             # FlipFlop
             fitness_fnc_FF = rh.FlipFlop()
@@ -184,7 +68,7 @@ def get_op_algo(algo):
 
             best_state_FF, best_fitness_FF, fitness_curve_FF = rh.simulated_annealing(problem=op_type_FF,
                                                                                 random_state=123, 
-                                                                                curve=True)
+                                                                                curve=True,)
             
             # Knapsack
             fitness_fnc_KS = rh.Knapsack(weights=[0.25, 0.50, 0.75, 1.0],
@@ -195,7 +79,7 @@ def get_op_algo(algo):
 
             best_state_KS, best_fitness_KS, fitness_curve_KS = rh.simulated_annealing(problem=op_type_KS,
                                                                                     random_state=123, 
-                                                                                    curve=True)
+                                                                                    curve=True,)
             
         case 'Genetic Algorithm':
             print(f"Running {algo}")
@@ -208,7 +92,7 @@ def get_op_algo(algo):
 
             best_state_FP, best_fitness_FP, fitness_curve_FP = rh.genetic_alg(problem=op_type_FP,
                                                                            random_state=123,
-                                                                           curve=True)
+                                                                           curve=True,)
 
             # FlipFlop
             fitness_fnc_FF = rh.FlipFlop()
@@ -218,7 +102,7 @@ def get_op_algo(algo):
 
             best_state_FF, best_fitness_FF, fitness_curve_FF = rh.genetic_alg(problem=op_type_FF,
                                                                          random_state=123,
-                                                                         curve=True)
+                                                                         curve=True,)
 
             # Knapsack
             fitness_fnc_KS = rh.Knapsack(weights=[0.25, 0.50, 0.75, 1.0],
@@ -229,7 +113,7 @@ def get_op_algo(algo):
 
             best_state_KS, best_fitness_KS, fitness_curve_KS = rh.genetic_alg(problem=op_type_KS,
                                                                              random_state=123,
-                                                                             curve=True)
+                                                                             curve=True,)
 
         case 'MIMIC':
             print(f"Running {algo}")
@@ -242,7 +126,7 @@ def get_op_algo(algo):
 
             best_state_FP, best_fitness_FP, fitness_curve_FP = rh.mimic(problem=op_type_FP,
                                                                      random_state=123,
-                                                                     curve=True)
+                                                                     curve=True,)
 
             # FlipFlop
             fitness_fnc_FF = rh.FlipFlop()
@@ -252,7 +136,7 @@ def get_op_algo(algo):
 
             best_state_FF, best_fitness_FF, fitness_curve_FF = rh.mimic(problem=op_type_FF,
                                                                    random_state=123,
-                                                                   curve=True)
+                                                                   curve=True,)
 
             # Knapsack
             fitness_fnc_KS = rh.Knapsack(weights=[0.25, 0.50, 0.75, 1.0],
@@ -263,7 +147,7 @@ def get_op_algo(algo):
 
             best_state_KS, best_fitness_KS, fitness_curve_KS = rh.mimic(problem=op_type_KS,
                                                                        random_state=123,
-                                                                       curve=True)
+                                                                       curve=True,)
             
     best_state =  {'best_state_FP': best_state_FP,
                    'best_state_FF': best_state_FF,
@@ -296,10 +180,7 @@ def main():
 
     X_train_scaled_cd, X_test_scaled_cd, y_train_cd, y_test_cd = final_dataset(dataset='cvd')
 
-    # nn = good_weights(X_train_scaled_cd, y_train_cd,
-    #                   X_test_scaled_cd, y_test_cd)
-
-    # opt_prob = good_problem()
+    opt_prob = good_problem()
 
 if __name__ == "__main__":
     main()

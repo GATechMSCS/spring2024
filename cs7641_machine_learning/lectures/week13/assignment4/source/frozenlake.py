8fdb9fda9dacdd54cd9ef0bbb929c10c9550344f
scottschmidl <sjschmidl@gmail.com>
2024-04-03 23:34:19 -0500

    made code reuseable

--- cs7641_machine_learning/lectures/week13/assignment4/source/frozenlake.py
+++ cs7641_machine_learning/lectures/week13/assignment4/source/frozenlake.py
@@ -11,25 +11,29 @@ from bettermdptools.utils.grid_search import GridSearch
 from gymnasium.envs.toy_text.frozen_lake import generate_random_map
 
 def make_env(mdp, size):
-    frozenlake = gym.make(id=mdp,
-                      desc=size,
-                      render_mode=None)
+    env = gym.make(id=mdp,
+                   desc=size,
+                   render_mode=None)
 
-    return frozenlake
+    return env
 
+def run_q_gridsearch(process, gamma, epsilon_decay, iters):
 
-def run_gridsearch(gamma, epsilon_decay, iters, size):
+    q_learning_results = GridSearch.q_learning_grid_search(env=process, gamma=gamma, epsilon_decay=epsilon_decay, iters=iters)    
 
-    size_mdp = generate_random_map(size=size)
-    mdp = 'FrozenLake8x8-v1'
+    return q_learning_results
 
-    frozenlake = make_env(mdp=mdp, size=size_mdp)
+def run_pi_gs(process, gamma, epsilon_decay, iters):
+    
+    pi_grid_results = GridSearch.pi_grid_search(env=process, gamma=gamma, n_iters=iters, theta=epsilon_decay)
 
-    q_learning_results = GridSearch.q_learning_grid_search(env=frozenlake, gamma=gamma, epsilon_decay=epsilon_decay, iters=iters)
-    pi_grid_results = GridSearch.pi_grid_search(env=frozenlake, gamma=gamma, n_iters=iters, theta=epsilon_decay)
-    vi_grid_results = GridSearch.vi_grid_search(env=frozenlake, gamma=gamma, n_iters=iters, theta=epsilon_decay)
+    return pi_grid_results
 
-    return q_learning_results#, pi_grid_results, vi_grid_results
+def run_vi_gs(process, gamma, epsilon_decay, iters):
+
+    vi_grid_results = GridSearch.vi_grid_search(env=process, gamma=gamma, n_iters=iters, theta=epsilon_decay)
+
+    return vi_grid_results
 
 #test policy
 # test_scores = TestEnv.test_env(env=frozenlake, n_iters=100, render=False, pi=pi_pi, user_input=False)
@@ -54,3 +58,33 @@ def run_gridsearch(gamma, epsilon_decay, iters, size):
 # title="FL Mapped Policy\nArrows represent best action"
 # val_max, policy_map = Plots.get_policy_map(pi, V, fl_actions, fl_map_size)
 # Plots.plot_policy(val_max, policy_map, fl_map_size, title)
+
+def main():
+
+    gamma = [0.95, 1.00, 1.05]
+    epsilon_decay = [0.55, 0.65, 0.75, 1.00]
+    iters = [25000, 50000]
+    size = generate_random_map(size=8)
+    mdp = 'FrozenLake8x8-v1'
+
+    frozenlake = make_env(mdp=mdp, size=size)
+
+    q_learning_results = run_q_gridsearch(process=frozenlake,
+                                      gamma=gamma,
+                                      epsilon_decay=epsilon_decay,
+                                      iters=iters)
+
+    q_learning_results = run_pi_gs(process=frozenlake,
+                               gamma=gamma, epsilon_decay=epsilon_decay,
+                               iters=iters)
+
+    pi_results = run_pi_gs(process=frozenlake,
+                               gamma=gamma, epsilon_decay=epsilon_decay,
+                               iters=iters)
+
+    vi_results = run_vi_gs(process=frozenlake,
+                               gamma=gamma, epsilon_decay=epsilon_decay, 
+                               iters=iters)
+    
+if __name__ == "__main__":
+    main()

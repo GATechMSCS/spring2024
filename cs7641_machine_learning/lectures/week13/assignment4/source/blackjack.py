33b1b453c8e7f1c11b91ff55bb3f74998b0d9d6a
scottschmidl <sjschmidl@gmail.com>
2024-04-03 21:59:03 -0500

    running through fl

--- cs7641_machine_learning/lectures/week13/assignment4/source/blackjack.py
+++ cs7641_machine_learning/lectures/week13/assignment4/source/blackjack.py
@@ -9,88 +9,62 @@ import seaborn as sns
 from bettermdptools.utils.decorators import add_to
 from bettermdptools.utils.grid_search import GridSearch
 from bettermdptools.utils.callbacks import MyCallbacks
+import matplotlib.pyplot as plt
+from gymnasium.spaces import Tuple, Discrete
 
-# FROM PLOTS NOTEBOOK
-#create env. 
-base_env = gym.make('Blackjack-v1', render_mode=None)
-blackjack = BlackjackWrapper(base_env)
-
-#run VI
-V, V_track, pi = Planner(blackjack.P).value_iteration()
-
-#create actions dictionary and set map size
-blackjack_actions = {0: "S", 1: "H"}
-blackjack_map_size=(29, 10)
-
-#get formatted state values and policy map
-val_max, policy_map = Plots.get_policy_map(pi, V, blackjack_actions, blackjack_map_size)
-
-#plot policy map
-title="Unedited\nBlackjack Policy Map"
-Plots.plot_policy(val_max, policy_map, blackjack_map_size, title)
-
-@add_to(Plots)
-@staticmethod
-def modified_plot_policy(val_max, directions, map_size, title):
-    """Plot the policy learned."""
-    sns.heatmap(
-        val_max,
-        annot=directions,
-        fmt="",
-        cmap=sns.color_palette("magma_r", as_cmap=True),
-        linewidths=0.7,
-        linecolor="black",
-    ).set(title=title)
-    img_title = f"Policy_{map_size[0]}x{map_size[1]}.png"
-    plt.show()
-
-title = "New Blackjack Policy Map"
-Plots.modified_plot_policy(val_max, policy_map, blackjack_map_size, title)
+mdp = 'Blackjack-v1'
+size = Tuple([Discrete(32), Discrete(11), Discrete(2)])
+gamma=[0.50, 0.75, .99]
+iters = [500, 750, 1000]
+theta=[.001, .00001]
+bl_base = gym.make(id=mdp,
+                    render_mode=None)
+bl_base.observation_space = size
+blackjack = BlackjackWrapper(bl_base)
 
 # FROM UTILITIES
-gamma=[.7, .9, .99]
-n_iters=[500]
-theta=[.001, .00001]
-base_env = gym.make('Blackjack-v1', render_mode=None)
-blackjack = BlackjackWrapper(base_env)
-GridSearch.vi_grid_search(blackjack, gamma, n_iters, theta)
+# @add_to(MyCallbacks)
+# def on_episode(self, caller, episode):
+#     if episode % 1000 == 0:
+#     	print(" episode=", episode)
 
-base_env = gym.make('Blackjack-v1', render_mode=None)
-blackjack = BlackjackWrapper(base_env)
+q_learning_results = GridSearch.q_learning_grid_search(blackjack, gamma, iters, theta)
+# pi_grid_results = GridSearch.pi_grid_search(blackjack, gamma, iters, theta)
+# vi_grid_results = GridSearch.vi_grid_search(blackjack, gamma, iters, theta)
 
-@add_to(MyCallbacks)
-def on_episode(self, caller, episode):
-    if episode % 1000 == 0:
-    	print(" episode=", episode)
+# #test policy
+# test_scores = TestEnv.test_env(env=blackjack, n_iters=100, render=False, pi=pi, user_input=False)
+# print(np.mean(test_scores))
 
-# Q-learning
-Q, V, pi, Q_track, pi_track = RL(blackjack).q_learning()
+# FROM PLOTS NOTEBOOK
+#run VI
+# V, V_track, pi = Planner(blackjack.P).value_iteration()
 
-# FROM FROZEN LAKE NOTEBOOK
-# make gym environment 
-frozen_lake = gym.make('FrozenLake8x8-v1', render_mode=None)
+#create actions dictionary and set map size
+# blackjack_actions = {0: "S", 1: "H"}
+# blackjack_map_size=(29, 10)
 
-# run VI
-V, V_track, pi = Planner(frozen_lake.P).value_iteration()
+#get formatted state values and policy map
+#val_max, policy_map = Plots.get_policy_map(pi, V, blackjack_actions, blackjack_map_size)
 
-#plot state values
-size=(8,8)
-Plots.values_heat_map(V, "Frozen Lake\nValue Iteration State Values", size)
+#plot policy map
+# title="Unedited\nBlackjack Policy Map"
+# Plots.plot_policy(val_max, policy_map, blackjack_map_size, title)
 
-# FROM BLACKJACK NOTEBOOK
-ase_env = gym.make('Blackjack-v1', render_mode=None)
-blackjack = BlackjackWrapper(base_env)
+# @add_to(Plots)
+# @staticmethod
+# def modified_plot_policy(val_max, directions, map_size, title):
+#     """Plot the policy learned."""
+#     sns.heatmap(
+#         val_max,
+#         annot=directions,
+#         fmt="",
+#         cmap=sns.color_palette("magma_r", as_cmap=True),
+#         linewidths=0.7,
+#         linecolor="black",
+#     ).set(title=title)
+#     img_title = f"Policy_{map_size[0]}x{map_size[1]}.png"
+#     plt.show()
 
-# run VI
-V, V_track, pi = Planner(blackjack.P).value_iteration()
-
-#test policy
-test_scores = TestEnv.test_env(env=blackjack, n_iters=100, render=False, pi=pi, user_input=False)
-print(np.mean(test_scores))
-
-# Q-learning
-Q, V, pi, Q_track, pi_track = RL(blackjack).q_learning()
-
-#test policy
-test_scores = TestEnv.test_env(env=blackjack, n_iters=100, render=False, pi=pi, user_input=False)
-print(np.mean(test_scores))
+# title = "New Blackjack Policy Map"
+# Plots.modified_plot_policy(val_max, policy_map, blackjack_map_size, title)
--- cs7641_machine_learning/lectures/week13/assignment4/source/frozenlake.py
+++ cs7641_machine_learning/lectures/week13/assignment4/source/frozenlake.py
@@ -8,30 +8,49 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from bettermdptools.utils.grid_search import GridSearch
+from gymnasium.envs.toy_text.frozen_lake import generate_random_map
+
+def make_env(mdp, size):
+    frozenlake = gym.make(id=mdp,
+                      desc=size,
+                      render_mode=None)
+
+    return frozenlake
+
+
+def run_gridsearch(gamma, epsilon_decay, iters, size):
+
+    size_mdp = generate_random_map(size=size)
+    mdp = 'FrozenLake8x8-v1'
+
+    frozenlake = make_env(mdp=mdp, size=size_mdp)
+
+    q_learning_results = GridSearch.q_learning_grid_search(env=frozenlake, gamma=gamma, epsilon_decay=epsilon_decay, iters=iters)
+    pi_grid_results = GridSearch.pi_grid_search(env=frozenlake, gamma=gamma, n_iters=iters, theta=epsilon_decay)
+    vi_grid_results = GridSearch.vi_grid_search(env=frozenlake, gamma=gamma, n_iters=iters, theta=epsilon_decay)
+
+    return q_learning_results#, pi_grid_results, vi_grid_results
+
+#test policy
+# test_scores = TestEnv.test_env(env=frozenlake, n_iters=100, render=False, pi=pi_pi, user_input=False)
+# print(np.mean(test_scores))
 
 # FROM PLOTS NOTEBOOK
 # state values heatmap
-frozen_lake = gym.make('FrozenLake8x8-v1', render_mode=None)
-V, V_track, pi = Planner(frozen_lake.P).value_iteration(n_iters=5000)
-size=(8,8)
-Plots.values_heat_map(V, "Frozen Lake\nValue Iteration State Values", size)
+# frozen_lake = gym.make('FrozenLake8x8-v1', render_mode=None)
+# V, V_track, pi = Planner(frozen_lake.P).value_iteration(n_iters=5000)
+# size=(8,8)
+# Plots.values_heat_map(V, "Frozen Lake\nValue Iteration State Values", size)
 
 # state values vs iterations
 # Clip trailing zeros in case convergence is reached before max iterations
 # This is likely when setting the n_iters parameter
-max_value_per_iter = np.trim_zeros(np.mean(V_track, axis=1), 'b')
-Plots.v_iters_plot(max_value_per_iter, "Frozen Lake\nMean Value v Iterations")
+# max_value_per_iter = np.trim_zeros(np.mean(V_track, axis=1), 'b')
+# Plots.v_iters_plot(max_value_per_iter, "Frozen Lake\nMean Value v Iterations")
 
 # policy maps
-fl_actions = {0: "←", 1: "↓", 2: "→", 3: "↑"}
-fl_map_size=(8,8)
-title="FL Mapped Policy\nArrows represent best action"
-val_max, policy_map = Plots.get_policy_map(pi, V, fl_actions, fl_map_size)
-Plots.plot_policy(val_max, policy_map, fl_map_size, title)
-
-# FROM UTILITIES
-frozen_lake = gym.make('FrozenLake8x8-v1', render_mode=None)
-gamma=[.99]
-epsilon_decay = [.9]
-iters = [500, 5000, 50000]
-GridSearch.q_learning_grid_search(frozen_lake, gamma, epsilon_decay, iters)
+# fl_actions = {0: "←", 1: "↓", 2: "→", 3: "↑"}
+# fl_map_size=(8,8)
+# title="FL Mapped Policy\nArrows represent best action"
+# val_max, policy_map = Plots.get_policy_map(pi, V, fl_actions, fl_map_size)
+# Plots.plot_policy(val_max, policy_map, fl_map_size, title)

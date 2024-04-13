6e8fc0dfbe3abdfff0374e95a4cec109f665fe3a
scottschmidl <sjschmidl@gmail.com>
2024-04-13 03:21:09 -0500

    got csvs for all L

--- cs7641_machine_learning/lectures/week13/assignment4/source/bj_plots.py
+++ cs7641_machine_learning/lectures/week13/assignment4/source/bj_plots.py
@@ -1,4 +1,3 @@
-from bettermdptools.algorithms.planner import Planner
 from bettermdptools.utils.plots import Plots
 import numpy as np
 import seaborn as sns
@@ -8,41 +7,42 @@ import matplotlib.pyplot as plt
 
 np.random.seed(123)
 
+# check
+Plots.values_heat_map
+Plots.v_iters_plot
+Plots.get_policy_map
+Plots.plot_policy
+
 # FROM UTILITIES
 @add_to(MyCallbacks)
 def on_episode(self, caller, episode):
     if episode % 1000 == 0:
     	print(" episode=", episode)
 
-# FROM PLOTS NOTEBOOK
-# run VI
-V, V_track, pi = Planner(blackjack.P).value_iteration()
+def bj_policy_map(pi, V):
+    # create actions dictionary and set map size
+    blackjack_actions = {0: "S", 1: "H"}
+    blackjack_map_size=(29, 10)
 
-# create actions dictionary and set map size
-blackjack_actions = {0: "S", 1: "H"}
-blackjack_map_size=(29, 10)
+    # get formatted state values and policy map
+    val_max, policy_map = Plots.get_policy_map(pi, V, blackjack_actions, blackjack_map_size)
 
-# get formatted state values and policy map
-val_max, policy_map = Plots.get_policy_map(pi, V, blackjack_actions, blackjack_map_size)
+    # plot policy map
+    title="Unedited\nBlackjack Policy Map"
+    Plots.plot_policy(val_max, policy_map, blackjack_map_size, title)
 
-# plot policy map
-title="Unedited\nBlackjack Policy Map"
-Plots.plot_policy(val_max, policy_map, blackjack_map_size, title)
+    title = "New Blackjack Policy Map"
+    Plots.modified_plot_policy(val_max, policy_map, blackjack_map_size, title)
 
 @add_to(Plots)
 @staticmethod
 def modified_plot_policy(val_max, directions, map_size, title):
     """Plot the policy learned."""
-    sns.heatmap(
-        val_max,
-        annot=directions,
-        fmt="",
-        cmap=sns.color_palette("magma_r", as_cmap=True),
-        linewidths=0.7,
-        linecolor="black",
-    ).set(title=title)
+    sns.heatmap(val_max,
+                annot=directions,
+                fmt="",
+                cmap=sns.color_palette("magma_r", as_cmap=True),
+                linewidths=0.7,
+                linecolor="black",).set(title=title)
     img_title = f"Policy_{map_size[0]}x{map_size[1]}.png"
     plt.show()
-
-title = "New Blackjack Policy Map"
-Plots.modified_plot_policy(val_max, policy_map, blackjack_map_size, title)
--- cs7641_machine_learning/lectures/week13/assignment4/source/fl_plots.py
+++ cs7641_machine_learning/lectures/week13/assignment4/source/fl_plots.py
@@ -1,4 +1,3 @@
-from bettermdptools.algorithms.planner import Planner
 from bettermdptools.utils.plots import Plots
 import numpy as np
 import seaborn as sns
@@ -8,22 +7,28 @@ import matplotlib.pyplot as plt
 
 np.random.seed(123)
 
-# FROM PLOTS NOTEBOOK
+Plots.values_heat_map
+Plots.v_iters_plot
+Plots.get_policy_map
+Plots.plot_policy
+
 # state values heatmap
-frozen_lake = gym.make('FrozenLake8x8-v1', render_mode=None)
-V, V_track, pi = Planner(frozen_lake.P).value_iteration(n_iters=5000)
-size=(8,8)
-Plots.values_heat_map(V, "Frozen Lake\nValue Iteration State Values", size)
+def fl_heatmap(size, V):
 
-# state values vs iterations
-# Clip trailing zeros in case convergence is reached before max iterations
-# This is likely when setting the n_iters parameter
-max_value_per_iter = np.trim_zeros(np.mean(V_track, axis=1), 'b')
-Plots.v_iters_plot(max_value_per_iter, "Frozen Lake\nMean Value v Iterations")
+    Plots.values_heat_map(V, "Frozen Lake\nValue Iteration State Values", size)
 
-# policy maps
-fl_actions = {0: "←", 1: "↓", 2: "→", 3: "↑"}
-fl_map_size=(8,8)
-title="FL Mapped Policy\nArrows represent best action"
-val_max, policy_map = Plots.get_policy_map(pi, V, fl_actions, fl_map_size)
-Plots.plot_policy(val_max, policy_map, fl_map_size, title)
+def fl_statevalues_iters(V_track):
+    
+    # state values vs iterations
+    # Clip trailing zeros in case convergence is reached before max iterations
+    # This is likely when setting the n_iters parameter
+    max_value_per_iter = np.trim_zeros(np.mean(V_track, axis=1), 'b')
+    Plots.v_iters_plot(max_value_per_iter, "Frozen Lake\nMean Value v Iterations")
+
+def fl_policy_map(pi, V):
+    # policy maps
+    fl_actions = {0: "←", 1: "↓", 2: "→", 3: "↑"}
+    fl_map_size=(8,8)
+    title="FL Mapped Policy\nArrows represent best action"
+    val_max, policy_map = Plots.get_policy_map(pi, V, fl_actions, fl_map_size)
+    Plots.plot_policy(val_max, policy_map, fl_map_size, title)

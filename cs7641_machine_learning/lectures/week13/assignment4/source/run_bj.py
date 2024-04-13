cda89799dcb1d55dfdbb6cadf66da41051c8bb48
scottschmidl <sjschmidl@gmail.com>
2024-04-13 04:50:57 -0500

    all algos converted to csv

--- cs7641_machine_learning/lectures/week13/assignment4/source/run_bj.py
+++ cs7641_machine_learning/lectures/week13/assignment4/source/run_bj.py
@@ -43,29 +43,29 @@ pathvi = "/home/leonardo_leads/Documents/SchoolDocs/ga_tech_masters/omscs_ml/spr
 ################ Q LEARNING ##################
 
 ################ GAMMA ##################
-# q_bj_gamma = run_ql_search(process=blackjack,
+# ql_bj_gamma = run_ql_search(process=blackjack,
 #                         gamma=gamma_ql_bj,
 #                         n_episodes=iters_ql_bj)
 
 # new_fields = {'V': np.append(ql_bj_gamma[0.9]['V'],
-#                              [0]*99744),
-#               'pi': list(ql_bj_gamma[0.9]['pi'].values())+[0]*99744,
+#                              [0]*99710),
+#               'pi': list(ql_bj_gamma[0.9]['pi'].values())+[0]*99710,
 #               'episode_rewards': ql_bj_gamma[0.9]['episode_rewards'],
 #               'average_episode_rewards': np.append(ql_bj_gamma[0.9]['average_episode_rewards'],
 #                                                   [0]*99999)}
 # pd.DataFrame(data=new_fields).to_csv(path_or_buf=pathql+'/ql_bj_gamma9.csv')
 
 # new_fields = {'V': np.append(ql_bj_gamma[0.99]['V'],
-#                              [0]*99744),
-#               'pi': list(ql_bj_gamma[0.99]['pi'].values())+[0]*99744,
+#                              [0]*99710),
+#               'pi': list(ql_bj_gamma[0.99]['pi'].values())+[0]*99710,
 #               'episode_rewards': ql_bj_gamma[0.99]['episode_rewards'],
 #               'average_episode_rewards': np.append(ql_bj_gamma[0.99]['average_episode_rewards'],
 #                                                   [0]*99999)}
 # pd.DataFrame(data=new_fields).to_csv(path_or_buf=pathql+'/ql_bj_gamma99.csv')
 
 # new_fields = {'V': np.append(ql_bj_gamma[0.999]['V'],
-#                              [0]*99744),
-#               'pi': list(ql_bj_gamma[0.999]['pi'].values())+[0]*99744,
+#                              [0]*99710),
+#               'pi': list(ql_bj_gamma[0.999]['pi'].values())+[0]*99710,
 #               'episode_rewards': ql_bj_gamma[0.999]['episode_rewards'],
 #               'average_episode_rewards': np.append(ql_bj_gamma[0.999]['average_episode_rewards'],
 #                                                   [0]*99999)}
@@ -76,25 +76,25 @@ pathvi = "/home/leonardo_leads/Documents/SchoolDocs/ga_tech_masters/omscs_ml/spr
 #                         epsilon_decay_ratio=epsilon_decay_ql_bj_edr,
 #                         n_episodes=iters_ql_bj)
 
-# new_fields = {'V': np.append(ql_bj_edr[0.8]['V'],
-#                              [0]*99984),
-#               'pi': list(ql_bj_edr[0.8]['pi'].values())+[0]*99984,
-#               'episode_rewards': ql_bj_edr[0.8]['episode_rewards'],
-#               'average_episode_rewards': np.append(ql_bj_edr[0.8]['average_episode_rewards'],
+# new_fields = {'V': np.append(ql_bj_edr[0.9]['V'],
+#                              [0]*99710),
+#               'pi': list(ql_bj_edr[0.9]['pi'].values())+[0]*99710,
+#               'episode_rewards': ql_bj_edr[0.9]['episode_rewards'],
+#               'average_episode_rewards': np.append(ql_bj_edr[0.9]['average_episode_rewards'],
 #                                                   [0]*99999)}
-# pd.DataFrame(data=new_fields).to_csv(path_or_buf=pathql+'/ql_bj_edr8.csv')
+# pd.DataFrame(data=new_fields).to_csv(path_or_buf=pathql+'/ql_bj_edr9.csv')
 
 # new_fields = {'V': np.append(ql_bj_edr[0.9]['V'],
-#                              [0]*99984),
-#               'pi': list(ql_bj_edr[0.9]['pi'].values())+[0]*99984,
+#                              [0]*99710),
+#               'pi': list(ql_bj_edr[0.9]['pi'].values())+[0]*99710,
 #               'episode_rewards': ql_bj_edr[0.9]['episode_rewards'],
 #               'average_episode_rewards': np.append(ql_bj_edr[0.9]['average_episode_rewards'],
 #                                                   [0]*99999)}
 # pd.DataFrame(data=new_fields).to_csv(path_or_buf=pathql+'/ql_bj_edr99.csv')
 
 # new_fields = {'V': np.append(ql_bj_edr[0.99]['V'],
-#                              [0]*99984),
-#               'pi': list(ql_bj_edr[0.99]['pi'].values())+[0]*99984,
+#                              [0]*99710),
+#               'pi': list(ql_bj_edr[0.99]['pi'].values())+[0]*99710,
 #               'episode_rewards': ql_bj_edr[0.99]['episode_rewards'],
 #               'average_episode_rewards': np.append(ql_bj_edr[0.99]['average_episode_rewards'],
 #                                                   [0]*99999)}
@@ -106,24 +106,24 @@ pathvi = "/home/leonardo_leads/Documents/SchoolDocs/ga_tech_masters/omscs_ml/spr
 #                         n_episodes=iters_ql_bj)
 
 # new_fields = {'V': np.append(ql_bj_alpha[0.3]['V'],
-#                              [0]*99984),
-#               'pi': list(ql_bj_alpha[0.3]['pi'].values())+[0]*99984,
+#                              [0]*99710),
+#               'pi': list(ql_bj_alpha[0.3]['pi'].values())+[0]*99710,
 #               'episode_rewards': ql_bj_alpha[0.3]['episode_rewards'],
 #               'average_episode_rewards': np.append(ql_bj_alpha[0.3]['average_episode_rewards'],
 #                                                   [0]*99999)}
 # pd.DataFrame(data=new_fields).to_csv(path_or_buf=pathql+'/ql_bj_alpha3.csv')
 
 # new_fields = {'V': np.append(ql_bj_alpha[0.5]['V'],
-#                              [0]*99984),
-#               'pi': list(ql_bj_alpha[0.5]['pi'].values())+[0]*99984,
+#                              [0]*99710),
+#               'pi': list(ql_bj_alpha[0.5]['pi'].values())+[0]*99710,
 #               'episode_rewards': ql_bj_alpha[0.5]['episode_rewards'],
 #               'average_episode_rewards': np.append(ql_bj_alpha[0.5]['average_episode_rewards'],
 #                                                   [0]*99999)}
 # pd.DataFrame(data=new_fields).to_csv(path_or_buf=pathql+'/ql_bj_alpha5.csv')
 
 # new_fields = {'V': np.append(ql_bj_alpha[0.7]['V'],
-#                              [0]*99984),
-#               'pi': list(ql_bj_alpha[0.7]['pi'].values())+[0]*99984,
+#                              [0]*99710),
+#               'pi': list(ql_bj_alpha[0.7]['pi'].values())+[0]*99710,
 #               'episode_rewards': ql_bj_alpha[0.7]['episode_rewards'],
 #               'average_episode_rewards': np.append(ql_bj_alpha[0.7]['average_episode_rewards'],
 #                                                   [0]*99999)}
@@ -132,31 +132,31 @@ pathvi = "/home/leonardo_leads/Documents/SchoolDocs/ga_tech_masters/omscs_ml/spr
 ################ POLICY ITERATION ##################
 
 ################ GAMMA ##################
-# bj_pi_gam = run_pi_search(process=frozenlakeL,
+# bj_pi_gamma = run_pi_search(process=blackjack,
 #                   gamma=gamma_pi_bj,
 #                   n_iters=iters_pi_bj)
 
-# new_fields = {'V': np.append(bj_pi_gam[0.9]['V'],
-#                              [0]*99744),
-#               'pi': list(bj_pi_gam[0.9]['pi'].values())+[0]*99744,
-#               'episode_rewards': bj_pi_gam[0.9]['episode_rewards'],
-#               'average_episode_rewards': np.append(bj_pi_gam[0.9]['average_episode_rewards'],
+# new_fields = {'V': np.append(bj_pi_gamma[0.9]['V'],
+#                              [0]*99710),
+#               'pi': list(bj_pi_gamma[0.9]['pi'].values())+[0]*99710,
+#               'episode_rewards': bj_pi_gamma[0.9]['episode_rewards'],
+#               'average_episode_rewards': np.append(bj_pi_gamma[0.9]['average_episode_rewards'],
 #                                                   [0]*99999)}
 # pd.DataFrame(data=new_fields).to_csv(path_or_buf=pathpi+'/bj_pi_gam9.csv')
 
-# new_fields = {'V': np.append(bj_pi_gam[0.99]['V'],
-#                              [0]*99744),
-#               'pi': list(bj_pi_gam[0.99]['pi'].values())+[0]*99744,
-#               'episode_rewards': bj_pi_gam[0.99]['episode_rewards'],
-#               'average_episode_rewards': np.append(bj_pi_gam[0.99]['average_episode_rewards'],
+# new_fields = {'V': np.append(bj_pi_gamma[0.99]['V'],
+#                              [0]*99710),
+#               'pi': list(bj_pi_gamma[0.99]['pi'].values())+[0]*99710,
+#               'episode_rewards': bj_pi_gamma[0.99]['episode_rewards'],
+#               'average_episode_rewards': np.append(bj_pi_gamma[0.99]['average_episode_rewards'],
 #                                                   [0]*99999)}
 # pd.DataFrame(data=new_fields).to_csv(path_or_buf=pathpi+'/bj_pi_gam99.csv')
 
-# new_fields = {'V': np.append(bj_pi_gam[0.999]['V'],
-#                              [0]*99744),
-#               'pi': list(bj_pi_gam[0.999]['pi'].values())+[0]*99744,
-#               'episode_rewards': bj_pi_gam[0.999]['episode_rewards'],
-#               'average_episode_rewards': np.append(bj_pi_gam[0.999]['average_episode_rewards'],
+# new_fields = {'V': np.append(bj_pi_gamma[0.999]['V'],
+#                              [0]*99710),
+#               'pi': list(bj_pi_gamma[0.999]['pi'].values())+[0]*99710,
+#               'episode_rewards': bj_pi_gamma[0.999]['episode_rewards'],
+#               'average_episode_rewards': np.append(bj_pi_gamma[0.999]['average_episode_rewards'],
 #                                                   [0]*99999)}
 # pd.DataFrame(data=new_fields).to_csv(path_or_buf=pathpi+'/bj_pi_gam999.csv')
 
@@ -192,58 +192,58 @@ pathvi = "/home/leonardo_leads/Documents/SchoolDocs/ga_tech_masters/omscs_ml/spr
 ################ VALUE ITERATION ##################
 
 ################ GAMMA ##################
-# bj_vi_gam = run_vi_search(process=frozenlakeL,
+# bj_vi_gamma = run_vi_search(process=blackjack,
 #                   gamma=gamma_vi_bj,
 #                   n_iters=iters_vi_bj)
 
-# new_fields = {'V': np.append(bj_vi_gam[0.9]['V'],
-#                              [0]*99744),
-#               'pi': list(bj_vi_gam[0.9]['pi'].values())+[0]*99744,
-#               'episode_rewards': bj_vi_gam[0.9]['episode_rewards'],
-#               'average_episode_rewards': np.append(bj_vi_gam[0.9]['average_episode_rewards'],
+# new_fields = {'V': np.append(bj_vi_gamma[0.9]['V'],
+#                              [0]*99710),
+#               'pi': list(bj_vi_gamma[0.9]['pi'].values())+[0]*99710,
+#               'episode_rewards': bj_vi_gamma[0.9]['episode_rewards'],
+#               'average_episode_rewards': np.append(bj_vi_gamma[0.9]['average_episode_rewards'],
 #                                                   [0]*99999)}
 # pd.DataFrame(data=new_fields).to_csv(path_or_buf=pathvi+'/bj_vi_gam9.csv')
 
-# new_fields = {'V': np.append(bj_vi_gam[0.99]['V'],
-#                              [0]*99744),
-#               'pi': list(bj_vi_gam[0.99]['pi'].values())+[0]*99744,
-#               'episode_rewards': bj_vi_gam[0.99]['episode_rewards'],
-#               'average_episode_rewards': np.append(bj_vi_gam[0.99]['average_episode_rewards'],
+# new_fields = {'V': np.append(bj_vi_gamma[0.99]['V'],
+#                              [0]*99710),
+#               'pi': list(bj_vi_gamma[0.99]['pi'].values())+[0]*99710,
+#               'episode_rewards': bj_vi_gamma[0.99]['episode_rewards'],
+#               'average_episode_rewards': np.append(bj_vi_gamma[0.99]['average_episode_rewards'],
 #                                                   [0]*99999)}
 # pd.DataFrame(data=new_fields).to_csv(path_or_buf=pathvi+'/bj_vi_gam99.csv')
 
-# new_fields = {'V': np.append(bj_vi_gam[0.999]['V'],
-#                              [0]*99744),
-#               'pi': list(bj_vi_gam[0.999]['pi'].values())+[0]*99744,
-#               'episode_rewards': bj_vi_gam[0.999]['episode_rewards'],
-#               'average_episode_rewards': np.append(bj_vi_gam[0.999]['average_episode_rewards'],
+# new_fields = {'V': np.append(bj_vi_gamma[0.999]['V'],
+#                              [0]*99710),
+#               'pi': list(bj_vi_gamma[0.999]['pi'].values())+[0]*99710,
+#               'episode_rewards': bj_vi_gamma[0.999]['episode_rewards'],
+#               'average_episode_rewards': np.append(bj_vi_gamma[0.999]['average_episode_rewards'],
 #                                                   [0]*99999)}
 # pd.DataFrame(data=new_fields).to_csv(path_or_buf=pathvi+'/bj_vi_gam999.csv')
 
 ################ THETA ##################
-# bj_vi_theta = run_vi_search(process=frozenlakeL,
+# bj_vi_theta = run_vi_search(process=blackjack,
 #                   theta=theta_vi_bj,
 #                   n_iters=iters_vi_bj)
 
 # new_fields = {'V': np.append(bj_vi_theta[1e-5]['V'],
-#                              [0]*99744),
-#               'pi': list(bj_vi_theta[1e-5]['pi'].values())+[0]*99744,
+#                              [0]*99710),
+#               'pi': list(bj_vi_theta[1e-5]['pi'].values())+[0]*99710,
 #               'episode_rewards': bj_vi_theta[1e-5]['episode_rewards'],
 #               'average_episode_rewards': np.append(bj_vi_theta[1e-5]['average_episode_rewards'],
 #                                                   [0]*99999)}
 # pd.DataFrame(data=new_fields).to_csv(path_or_buf=pathvi+'/bj_vi_theta5.csv')
 
 # new_fields = {'V': np.append(bj_vi_theta[1e-7]['V'],
-#                              [0]*99744),
-#               'pi': list(bj_vi_theta[1e-7]['pi'].values())+[0]*99744,
+#                              [0]*99710),
+#               'pi': list(bj_vi_theta[1e-7]['pi'].values())+[0]*99710,
 #               'episode_rewards': bj_vi_theta[1e-7]['episode_rewards'],
 #               'average_episode_rewards': np.append(bj_vi_theta[1e-7]['average_episode_rewards'],
 #                                                   [0]*99999)}
 # pd.DataFrame(data=new_fields).to_csv(path_or_buf=pathvi+'/bj_vi_theta7.csv')
 
 # new_fields = {'V': np.append(bj_vi_theta[1e-9]['V'],
-#                              [0]*99744),
-#               'pi': list(bj_vi_theta[1e-9]['pi'].values())+[0]*99744,
+#                              [0]*99710),
+#               'pi': list(bj_vi_theta[1e-9]['pi'].values())+[0]*99710,
 #               'episode_rewards': bj_vi_theta[1e-9]['episode_rewards'],
 #               'average_episode_rewards': np.append(bj_vi_theta[1e-9]['average_episode_rewards'],
 #                                                   [0]*99999)}
